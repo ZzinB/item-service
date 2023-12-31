e5f19117469de6d229bef7f26129c5bdf27a84d6
ZzinB <mysteleee7@gmail.com>
2023-12-31 17:45:36 +0900

    Feat : 추가 업데이트기능

--- src/main/java/hello/itemservice/domain/item/ItemRepository.java
+++ src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -28,4 +28,11 @@ public class ItemRepository {
         return new ArrayList<>(store.values());
     }
 
+    public void update(Long itemId, Item updateParam) {
+        Item findItem = findById(itemId);
+        findItem.setItemName(updateParam.getItemName());
+        findItem.setPrice(updateParam.getPrice());
+        findItem.setQuantity(updateParam.getQuantity());
+    }
+
 }

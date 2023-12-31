51908babb3c56028ce3c54a1bad38355eaad05cc
ZzinB <mysteleee7@gmail.com>
2023-12-31 17:41:52 +0900

    Feat : 추가 저장기능

---
+++ src/main/java/hello/itemservice/domain/item/ItemRepository.java
@@ -0,0 +1,22 @@
+package hello.itemservice.domain.item;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+//package 나눠도 되지만, 일단 규모가 작으니까 분리 안함 !
+@Repository
+public class ItemRepository {
+
+    private static final Map<Long, Item> store = new HashMap<>(); //static 사용
+    private static long sequence = 0L; //static 사용
+
+
+    public Item save(Item item){
+        item.setId(++sequence);
+        store.put(item.getId(), item);
+        return item;
+    }
+
+}
